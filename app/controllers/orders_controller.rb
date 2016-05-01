3fab06cc9389ba3f61d4461c9b67832a16bdf792
Enzo Zerega <eczerega@MacBook-Pro-de-Enzo-2.local>
2016-05-01 17:12:12 -0300

    asdasdasd

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,30 @@
+require 'base64'
+require 'cgi'
+require 'openssl'
+require 'net/http'
+#permite generar el hash para las distintas autorizaciones, lo retorna
+
+
+
 class OrdersController < ApplicationController
 
-	  layout false
+	layout false
+	def generateHash (contenidoSignature)
+		encoded_string = Base64.encode64(OpenSSL::HMAC.digest('sha1','akVf0btGVOwkhvI', contenidoSignature)).chomp
+		return encoded_string
+	end
 
 	def index
+		@hashi = 'INTEGRACION grupo12:'+generateHash('GET'+'').to_s
+		puts @hashi
+		var_url_get="http://integracion-2016-dev.herokuapp.com/bodega/almacenes"
+		uri = URI.parse(var_url_get)
+		http = Net::HTTP.new(uri.host, uri.port)
+		request = Net::HTTP::Get.new(uri.request_uri)
+		request['Authorization'] = @hashi
+		response = http.request(request).to_json
+		puts response
+	  #@response = JSON.parse RestClient.get data_ur_almacenes, {:Authorization => @hashi}
 	end
 
 
