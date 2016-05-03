17a6c220575ee9c5b3d929a9d910e83b731441b2
happyhairfish <felipe.lr7@gmailcom>
2016-05-03 00:23:49 -0300

    up

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -139,6 +139,5 @@ class OrdersController < ApplicationController
         format.json {  }
       end
 	end
-
 end
 #rails g scaffold oc_recibidas id_dev:string created_at_dev:date canal:string sku:string cantidad:integer precio_unit:integer entrega_at:date despacho_at:date estado:string rechazo:string anulacion:string notas:string id_factura_dev:string
